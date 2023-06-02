4634ddfcbb04014b925719bf52e795518d9ac15e
DNQGithub <dnq.httt@gmail.com>
2023-06-02 18:16:42 +0700

    Update responsive error 500 page

--- src/pages/error-500-page.tsx
+++ src/pages/error-500-page.tsx
@@ -1,6 +1,17 @@
-import { Box, Center, Container, Image, Text } from '@mantine/core';
+import { Box, Container, Image, MediaQuery, Text, createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  message: {
+    [theme.fn.smallerThan('md')]: {
+      fontSize: 20,
+      lineHeight: '2rem',
+    },
+  },
+}));
 
 export const Error500Page = () => {
+  const { classes } = useStyles();
+
   return (
     <Box
       pt={120}
@@ -12,15 +23,34 @@ export const Error500Page = () => {
       }}
     >
       <Container>
-        <Box px={100} py={50}>
-          <Image
-            src="images/decor/under-construction.png"
-            width={554}
-            height={346}
-            mx="auto"
-            mb={61}
-          />
-          <Text ff="Outfit" fw={500} size={30} color="dark.4" ta="center" lh="2.35rem">
+        <Box px={{ base: 20, md: 100 }} py={50}>
+          <MediaQuery largerThan="md" styles={{ display: 'none' }}>
+            <Image
+              src="images/decor/under-construction.png"
+              width={300}
+              height={'auto'}
+              mx="auto"
+              mb={61}
+            />
+          </MediaQuery>
+          <MediaQuery smallerThan="md" styles={{ display: 'none' }}>
+            <Image
+              src="images/decor/under-construction.png"
+              width={554}
+              height={346}
+              mx="auto"
+              mb={61}
+            />
+          </MediaQuery>
+          <Text
+            ff="Outfit"
+            fw={500}
+            size={30}
+            color="dark.4"
+            ta="center"
+            lh="2.35rem"
+            className={classes.message}
+          >
             Our dedicated team is diligently working towards the launch of an exceptional product.
             Stay tuned for upcoming announcements as we have even more exciting things to reveal in
             the near future.
