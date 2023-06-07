58339609e7716dcfd4072134992d6133411e2c27
DNQGithub <dnq.httt@gmail.com>
2023-06-07 19:15:51 +0700

    Rename page fotress

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export * from './home-page/home-page';
 export * from './gm-ecosystem-page/gm-ecosystem-page';
-export * from './fortless-page';
+export * from './fotress-page';
 export * from './tou-page';
 export * from './error-500-page';
