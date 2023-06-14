952fe86aa21feffbfc61ef59bc7ce0fd585d1174
DNQGithub <dnq.httt@gmail.com>
2023-06-14 11:27:33 +0700

    Update roadmap.tsx

--- src/pages/home-page/roadmap.tsx
+++ src/pages/home-page/roadmap.tsx
@@ -10,59 +10,74 @@ export const RoadMap = () => {
           size={40}
           color="sky.4"
           ta={{ base: 'center', md: 'right' }}
-          mb={50}
+          mb={15}
         >
           Product Roadmap
         </Text>
 
-        <Flex direction={{ base: 'column', sm: 'row' }} justify="center" gap={50}>
-          <Flex
-            direction="column"
-            gap={10}
-            w={{ base: '100%', sm: 'calc(100% / 3 - 3.125rem)' }}
-            style={{ borderRadius: 12, overflow: 'hidden' }}
-          >
-            <Center bg="purpleGlow.1" h={450} px={38} py={100}>
-              <Image maw={250} src="images/roadmap/road-1.png" fit="contain" />
-            </Center>
-            <Box bg="purpleGlow.2" py={10} px={20}>
-              <Text ff="Open Sans" fw={400} ta="center">
-                Matrix-powered Decentralized Chat with in-app BTC-EVM wallet
-              </Text>
-            </Box>
-          </Flex>
+        <Text
+          ff="Open Sans"
+          fw={400}
+          size={14}
+          color="dark.4"
+          lh="1.25rem"
+          ta={{ base: 'center', md: 'right' }}
+          mb={90}
+        >
+          ChatGM's product journey begins with Phase One, where we develop a Matrix-powered
+          decentralized chat platform with an integrated wallet supporting Bitcoin and
+          EVM-compatible assets. Moving onto Phase Two, we enhance our platform's functionality by
+          introducing a Decentralized Exchange (DEX) aggregator, enabling users to access and
+          compare rates from various exchanges. The journey culminates with Phase Three, adding
+          Peer-to-Peer (P2P) capabilities for direct user interactions and transactions, reinforcing
+          ChatGM's commitment to creating a secure, decentralized community.
+        </Text>
 
-          <Flex
-            direction="column"
-            gap={10}
-            w={{ base: '100%', sm: 'calc(100% / 3 - 3.125rem)' }}
-            style={{ borderRadius: 12, overflow: 'hidden' }}
-          >
-            <Center bg="saffron.1" h={450} px={38} py={100}>
-              <Image maw={250} src="images/roadmap/road-2.png" fit="contain" />
-            </Center>
-            <Box bg="saffron.3" py={10} px={20}>
-              <Text ff="Open Sans" fw={400} ta="center">
+        <Flex direction={{ base: 'column', sm: 'row' }} justify="center" gap={40}>
+          <Box style={{ flex: 1 }}>
+            <Box p={10} mb={40} bg="rose.3" style={{ borderRadius: 12 }}>
+              <Text ff="Open Sans" fw={700} size={14} ta="center" color="saffron.1">
+                Version 1
+              </Text>
+              <Text ff="Outfit" fw={400} size={18} ta="center" color="white.1">
                 Matrix-powered Decentralized Chat with in-app BTC-EVM wallet
               </Text>
+              <Text ff="Open Sans" fw={700} size={14} ta="center" color="saffron.1">
+                Q2 2023
+              </Text>
             </Box>
-          </Flex>
+            <Image src="images/roadmap/road-1.png" height={340} fit="contain" />
+          </Box>
 
-          <Flex
-            direction="column"
-            gap={10}
-            w={{ base: '100%', sm: 'calc(100% / 3 - 3.125rem)' }}
-            style={{ borderRadius: 12, overflow: 'hidden' }}
-          >
-            <Center bg="sky.1" h={450} px={38} py={100}>
-              <Image maw={250} src="images/roadmap/road-3.png" fit="contain" />
-            </Center>
-            <Box bg="sky.3" py={10} px={20}>
-              <Text ff="Open Sans" fw={400} ta="center">
-                Matrix-powered Decentralized Chat with in-app BTC-EVM wallet
+          <Box style={{ flex: 1 }}>
+            <Box p={10} mb={40} bg="purpleGlow.3" style={{ borderRadius: 12 }}>
+              <Text ff="Open Sans" fw={700} size={14} ta="center" color="saffron.1">
+                Version 2
+              </Text>
+              <Text ff="Outfit" fw={400} size={18} ta="center" color="white.1">
+                Multicurrency Peer-to-Peer OTC Exchange for Bitcoin & Ethereum
+              </Text>
+              <Text ff="Open Sans" fw={700} size={14} ta="center" color="saffron.1">
+                Q3 2023
               </Text>
             </Box>
-          </Flex>
+            <Image src="images/roadmap/road-2.png" height={340} fit="contain" />
+          </Box>
+
+          <Box style={{ flex: 1 }}>
+            <Box p={10} mb={40} bg="sky.4" style={{ borderRadius: 12 }}>
+              <Text ff="Open Sans" fw={700} size={14} ta="center" color="saffron.1">
+                Version 3
+              </Text>
+              <Text ff="Outfit" fw={400} size={18} ta="center" color="white.1">
+                Multichain Decentralized Exchange (DEX) Aggregator
+              </Text>
+              <Text ff="Open Sans" fw={700} size={14} ta="center" color="saffron.1">
+                Q4 2023
+              </Text>
+            </Box>
+            <Image src="images/roadmap/road-3.png" height={340} fit="contain" />
+          </Box>
         </Flex>
       </Container>
     </Box>
