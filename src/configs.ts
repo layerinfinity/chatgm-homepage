9b19bb9cd61f7e0403ad68cdd69b60328af41d2c
DNQGithub <dnq.httt@gmail.com>
2023-06-28 11:05:50 +0700

    Update content

--- src/configs.ts
+++ src/configs.ts
@@ -8,6 +8,6 @@ export const ANDROID_STORE_URL = 'https://play.google.com/store/apps/details?id=
 export const IOS_STORE_URL = 'https://testflight.apple.com/join/nTMNxBbK';
 
 export const DISCORD_URL = 'https://discord.gg/m4MMtjT7';
-export const TWITTER_URL = 'https://twitter.com/Chatgm_Crypto';
+export const TWITTER_URL = 'https://twitter.com/ChatGM_Official';
 export const FACEBOOK_URL = 'https://www.facebook.com/ChatGMSocialxCrypto';
-export const MEDIUM_URL = 'https://medium.com/@ethan_4414';
+export const MEDIUM_URL = 'https://medium.com/@chatGM';
