3fdc1364b0cf773bffe7ea171951bb6954f1ce7e
candangios <candd1707@gmail.com>
2024-06-26 10:56:25 +0700

    Update top-header.tsx

--- src/pages/home-page/sections/top-header.tsx
+++ src/pages/home-page/sections/top-header.tsx
@@ -1,6 +1,5 @@
 import { Box, Container, Flex, Image, Text, createStyles, useMantineTheme } from '@mantine/core';
-
-
+import Typewriter from 'typewriter-effect';
 const useStyles = createStyles((theme) => ({
     heading: {
         [theme.fn.smallerThan('md')]: {
@@ -32,14 +31,48 @@ export const TopHeader = () => {
                 background: theme.fn.linearGradient(0, '#6016D900', '#000000ff'),
             }}
         >
+            <Box
+
+                h={793}
+                style={{
+                    background: theme.fn.radialGradient('#8D57FF66', '#8657FF0d', '#8357FF00'),
+                }}>
+
+            </Box>
             <Container py={80} >
+
                 <Flex direction='column' align='center' mt={80} >
                     <Image src='images/one-chainai/white1chain.png' width={236} fit="contain" />
-                    <Text ff="Outfit" fw={400} size={18} color="white.0" ta="center">
-                        1ChainAi
+                    <Text ff="Outfit" fw={500} size={30} color="white.0" ta="center" mt={44}>
+                        <Typewriter
+                            // onInit={(typewriter) => {
+                            //     typewriter.typeString('Simplify Web3 Adoption with Interactive AI_')
+                            //         .callFunction(() => {
+                            //             console.log('String typed out!');
+                            //         })
+
+                            //         .pauseFor(2500)
+                            //         .deleteAll()
+                            //         .callFunction(() => {
+                            //             console.log('All strings were deleted');
+                            //         })
+
+                            //         .start();
+
+                            // }}
+                            options={{
+                                strings: ['1Chain AI - Simplify Web3 Mass Adoption with AI'],
+                                autoStart: true,
+                                loop: true,
+                                delay: 50,
+                            }}
+                        />
                     </Text>
 
+
                 </Flex>
+
+
 
 
 
